6ec6f2c446a1c8c6642ed8ff0798677b81d5b7c0
serg-akulov <serg.akulov74@gmail.com>
2025-12-18 10:28:21 +0700

    dell auto

--- auto/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-    // true = Полная версия (АвтоРевизор PRO)
-    // false = Бесплатная версия (АвтоРевизор Lite)
-    isProVersion: false, 
-
-    // Ссылка на PRO версию в RuStore (замени, когда опубликуешь)
-    proAppUrl: 'https://apps.rustore.ru/app/com.yourname.autorevizor.pro' 
-};
